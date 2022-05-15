d419d1f4b7d9fb1dfd4925f19d47647bbf326a1a
Kholoud Saad <kholoudsaad1@outlook.com>
2022-05-15 14:46:23 +0200

    adding event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import AddEvent from './component/AddEvent';
-import DisplayEvent from './component/DisplayEvent';
-import { EventContext } from './component/EventsContext';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <DisplayEvent /> */}
       <AddEvent />
-      {/* <EventContext /> */}
     </div>
   );
 }
